8cffef2bd93a6471a47601b22ed768127549b2af
Vinoth Pandian <vinothpandian@live.com>
2019-01-14 18:25:45 +0100

    Updates

---
+++ precache-manifest.ad130a89c15bdbcbd11602e992386c16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2583f8f0d43c3b71274",
+    "url": "/cra-redux-saga/static/css/main.b1acb2ce.chunk.css"
+  },
+  {
+    "revision": "a2583f8f0d43c3b71274",
+    "url": "/cra-redux-saga/static/js/main.a2583f8f.chunk.js"
+  },
+  {
+    "revision": "aebc2aefde13fbf83447",
+    "url": "/cra-redux-saga/static/css/1.92441794.chunk.css"
+  },
+  {
+    "revision": "aebc2aefde13fbf83447",
+    "url": "/cra-redux-saga/static/js/1.aebc2aef.chunk.js"
+  },
+  {
+    "revision": "8f2d083d0604c66e4bfb",
+    "url": "/cra-redux-saga/static/js/runtime~main.8f2d083d.js"
+  },
+  {
+    "revision": "ec635d0b12029065c1da586cd3e900f3",
+    "url": "/cra-redux-saga/index.html"
+  }
+];
